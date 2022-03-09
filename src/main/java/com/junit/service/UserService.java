f8d6d229dcdf072b2bd7b0e979a8b9de770346a4
Aram <aram050385@gmail.com>
2022-03-09 14:56:08 +0400

    begin

---
+++ src/main/java/com/junit/service/UserService.java
@@ -0,0 +1,15 @@
+package com.junit.service;
+
+
+import com.junit.dto.User;
+
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class UserService {
+    public List<User> getAll() {
+        return Collections.emptyList();
+    }
+}
